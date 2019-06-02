dc62d8372c6842189e8fbcaf712e034f726b0114
shengjibanwoailuo <1048496188@qq.com>
2019-06-02 16:36:40 +0800

    最大数

--- algorithm/traning/leetcode/179.go
+++ algorithm/traning/leetcode/179.go
@@ -1,1 +1,26 @@
 package main
+import (
+	"fmt"
+	"sort"
+)
+
+func largestNumber(nums []int) string {
+	sort.Slice(nums,func (i int, j int) bool {
+		return fmt.Sprintf("%d",nums[i]) + fmt.Sprintf("%d",nums[j]) > fmt.Sprintf("%d",nums[j]) + fmt.Sprintf("%d",nums[i])
+	})
+	var res string
+	if nums[0] == 0 {
+		res += "0"
+		return res
+	}
+	for i := 0; i < len(nums); i++ {
+		res += fmt.Sprintf("%d",nums[i])
+	}
+
+	return res
+}
+func main() {
+	arr := []int{3,30,34,5,9}
+	res := largestNumber(arr)
+	fmt.Print(res)
+}

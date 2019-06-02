3484cdc835313304a8ff524196a92f6127497ff2
shengjibanwoailuo <1048496188@qq.com>
2019-06-02 16:38:00 +0800

    两个数组的交集

--- algorithm/traning/leetcode/349.go
+++ algorithm/traning/leetcode/349.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func intersection(nums1 []int, nums2 []int) []int {
 	len1 := len(nums1)
@@ -15,22 +17,19 @@ func intersection(nums1 []int, nums2 []int) []int {
 	}else {
 		min = len1
 	}
-	var res = make([]int,1,min)
+	var res []int
 	for ; i < min; i++ {
 		map1[nums1[i]]++
 		map2[nums2[i]]++
 	}
-	var t = 0
 	if len1 >= len2 {
 		for ; i < len1; i++ {
 			map1[nums1[i]]++
 		}
 		for k,_ := range map2 {
 			if map1[k] > 0 {
-				fmt.Println(k,"  ",map1[k])
-			res[t] = k
-			t++
-		}
+				res = append(res,k)
+			}
 		}
 		return res
 	}else {
@@ -39,10 +38,8 @@ func intersection(nums1 []int, nums2 []int) []int {
 		}
 		for k,_ := range map1 {
 			if map2[k] > 0 {
-				fmt.Println(k,"  ",map2[k])
-			res[t] = k
-			t++
-		}
+				res = append(res,k)
+			}
 		}
 		return res
 	}
